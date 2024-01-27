43e09eedb86bb8e9de8e3de80c898c50891ce429
chikki30 <charan991991@gmail.com>
2024-01-27 17:37:45 -0500

    pratice1

---
+++ pratice.java
@@ -0,0 +1,12 @@
+public class pratice {
+    public static void main(String[] args) {
+
+
+
+        String str1="www.udemy.co.in";
+        System.out.println(str1.indexOf("."));
+        System.out.println(str1.indexOf(".",4));
+        System.out.println(str1.indexOf("udemy"));
+        System.out.println(str1.lastIndexOf("."));
+    }
+}
