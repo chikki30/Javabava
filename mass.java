fdbe91e14d80dacda6bef9aca2f714f1e7fd3807
chikki30 <charan991991@gmail.com>
2024-01-27 17:36:19 -0500

    pratice1

--- mass.java
+++ mass.java
@@ -1,30 +1,14 @@
-import java.util.*;
-
 public class mass {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Enter a URL");
-        String url = scan.nextLine();
-
-        String protocol = url.substring(0, url.indexOf(":"));
-
-        if (protocol.equals("http"))
-            System.out.println("Hypertext Transfer Protocol");
-        else if (protocol.equals("ftp"))
-            System.out.println("File Transfer Protocol");
 
 
-        String ext = url.substring(url.lastIndexOf(".") + 1);
 
-        if (ext.equals("com"))
-            System.out.println("Commercial");
-        else if (ext.equals("org"))
-            System.out.println("Organisation");
-        else if (ext.equals("net"))
-            System.out.println("Network");
-
-
+        String str1="www.udemy.co.in";
+        System.out.println(str1.indexOf("."));
+        System.out.println(str1.indexOf(".",4));
+        System.out.println(str1.indexOf("udemy"));
+        System.out.println(str1.lastIndexOf("."));
     }
+
 }
