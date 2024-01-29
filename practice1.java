a62c6ddfdcf3fe351614c6fa9da10de594f9008e
chikki30 <charan991991@gmail.com>
2024-01-28 19:47:46 -0500

    pratice1

---
+++ practice1.java
@@ -0,0 +1,13 @@
+public class StringPractice3 {
+
+    public static void main(String[] args) {
+
+
+        String str1="the great wall ";
+        String str2="of china";
+        System.out.println(str1.contains("wall"));
+        System.out.println(str1.concat(str2));//System.out.println(str1 + str2);
+
+    }
+
+}
