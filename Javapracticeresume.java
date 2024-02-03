add30b0a602e64ca04466f256cc03d84f0fb1c83
chikki30 <charan991991@gmail.com>
2024-02-03 15:22:46 -0500

    compare

---
+++ Javapracticeresume.java
@@ -0,0 +1,12 @@
+public class Javapracticeresume
+{
+    //Cheking if a string is Binary number
+    public static void main(String[] args) {
+        int b = 100110010;
+
+        String str = String.valueOf(b);
+
+        System.out.println(str.matches("[01]+"));
+
+    }
+    }
