a6c54d28d8dc86cc524a92cfb4553d20bdc40d0c
chikki30 <charan991991@gmail.com>
2024-02-23 14:30:22 -0500

    MethodPractice

---
+++ methodpractice2/Methodpractice2.java
@@ -0,0 +1,26 @@
+package methodpractice2;
+
+public class Methodpractice2
+{
+    static boolean isPrime(int n)
+    {
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args)
+    {
+
+            System.out.println(isPrime(15));
+
+
+
+    }
+
+}
+
